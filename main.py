f6f44845b3ef4b32dc4ff76e3c6f94a3a14d2321
Feisal Aden Darar <feisal_darar@hotmail.com>
2023-05-13 00:31:17 -0400

    updates

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import csv
 import pandas as pd 
+import codecs
+import unicodecsv
+import yaml
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import (
     DateRange,
@@ -12,6 +16,30 @@ from google.analytics.data_v1beta.types import (
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "credentials.json"
 client = BetaAnalyticsDataClient()
+country = yaml.full_load(open('reg.yml', 'r', encoding='utf-8'))
+country_name = country.get('country_region').get('country')
+region_name = country.get('country_region').get('region')
+def write_csv(filename, rows, header=None):
+    outf=open(filename, 'wb')
+    outf.write(codecs.BOM_UTF8)
+    writer = unicodecsv.writer(outf)
+
+    if header:
+        writer.writerow(header)
+    for row in rows:
+        writer.writerow(row)
+
+def read_csv(filename):
+    content=[]
+    with open(filename, encoding='UTF-8') as f:
+        reader = csv.reader(f)
+        firstrow = next(reader)
+        #firstrow[0] = firstrow[0].lstrip(codecs.BOM_UTF8)
+        content.append(firstrow)
+        for x in reader:
+            content.append(x)
+    return content
+# parsing GA4 report to align with our reporting
 def parseReport(response, dimension_name='pagePath', metric_name='eventCount'):
     data, rows_returned = [], 0
     
@@ -19,8 +47,9 @@ def parseReport(response, dimension_name='pagePath', metric_name='eventCount'):
         
         dim, mtr = [],[]
         if dimension_name:
-            for i, dimension_value in enumerate(row.dimension_values):            
-                dim.append(dimension_value.value)         
+            for i, dimension_value in enumerate(row.dimension_values): 
+                if response.dimension_headers[i].name == dimension_name:          
+                    dim.append(dimension_value.value)         
 
         for i, metric_value in enumerate(row.metric_values):            
             mtr.append(metric_value.value)
@@ -30,7 +59,72 @@ def parseReport(response, dimension_name='pagePath', metric_name='eventCount'):
              data.append(mtr)
     rows_returned = response.row_count
     return data, rows_returned
-def monthly_usage( start, end, csv_file=None,property_id="359129908"):
+def by_country(start,end,property_id="359129908", csv_file= None):
+    request = RunReportRequest(
+    property=f"properties/{property_id}",
+    dimensions= [Dimension(name="country")],
+    metrics=[Metric(name="sessions")],
+    date_ranges=[DateRange(start_date= start, end_date=end)]
+        , 
+    # dimension_filter  = FilterExpression(
+    #     filter = Filter (
+    #         field_name ='eventName',
+    #         string_filter = Filter.StringFilter(value = 'file_download')
+    #     )
+
+    # ),
+    limit = 100000,
+    offset = 0,
+
+        
+    )
+    response = client.run_report(request) 
+    data, rows_retuned = parseReport(response, 'country', 'sessions')
+        # body={
+        #     'reportRequests': [
+        #         {
+        #           'viewId': VIEW_ID,
+        #           'dateRanges': [{'startDate': '2012-01-01', 'endDate': end}],
+        #               'metrics': [{'expression': 'ga:sessions'} ],
+        #           'dimensions':[{'name':'ga:country'}],
+
+        #           'orderBys':[
+        #             {'fieldName': 'ga:sessions',
+        #              'sortOrder': 'DESCENDING'
+        #             }],
+        #            'pageToken': '0',
+        #            'pageSize': '1000',
+        #         }]
+        #     }
+        # response = analytics.reports().batchGet(body=body
+        #     ).execute()
+    
+    total = 0
+    country_fr = [c for c in country_name.values()]
+    country_en = [c for c in country_name.keys()]
+    assert( len(country_en)==len(country_fr))
+    country_dict = { country_en[i]:country_fr[i] for i in range(len(country_en)) }
+
+    for row in data:
+        c = row[0]
+        if c == '(not set)':
+            row[0] = 'unknown / Inconnu'
+        else:
+            c_fr = country_dict.get(c, c)
+            row[0] = c + u' | ' + c_fr
+        row[1] = int(row[1])
+
+    for c, count in data:
+        total += count
+        #print(count, total)
+    data = [ [country, int(count), "%.2f"%((count*100.0)/total) + '%' ] for [country, count] in data ]
+    df = pd.DataFrame(data,columns=['region / Région', 'visits / Visites', 'percentage of total visits / Pourcentage du nombre total de visites'])
+    data.insert(0,['region / Région', 'visits / Visites', 'percentage of total visits / Pourcentage du nombre total de visites'])
+    print(df.head())        
+    #write_csv(csv_file, data)
+
+# Total visits and downloads within a given period 
+def total_usage( start, end, csv_file,property_id="359129908"):
         total, downloads = 0, 0        
         request = RunReportRequest(
         property=f"properties/{property_id}",
@@ -72,8 +166,19 @@ def monthly_usage( start, end, csv_file=None,property_id="359129908"):
         data, rows_retuned = parseReport(response, None, 'eventCount')         
         for eCount , eVal in data:
             downloads += int(eCount)
+        #print(f"Visits:{total} and Downloads:  {downloads}")
+        # Checking if the report is up to date and updating otherwise
+        [year, month, _] = start.split('-')
+        data = read_csv(csv_file)
+        if int(data[1][0]) == int(year) and int(data[1][1]) == int(month):
+            print ('entry exists, no overwriting')
+            return
+        row = [year, month, total, downloads]
+        data[0] = ['year / année', 'month / mois', 'visits / visites', 'downloads / téléchargements']
+        data.insert(1,row)
+        write_csv(csv_file, data)
         
-    
+        print(data)
 
 def getRawReport(start, end, property_id="359129908"):
     """Runs a simple report on a Google Analytics 4 property."""
@@ -89,7 +194,7 @@ def getRawReport(start, end, property_id="359129908"):
     request = RunReportRequest(
         property=f"properties/{property_id}",
         dimensions= [Dimension(name="eventName"),
-                     Dimension(name="PagePath")
+                     Dimension(name="pagePath")
                      ],
         metrics=[Metric(name="eventValue"), 
                  Metric(name="eventCount")],
@@ -107,9 +212,10 @@ def getRawReport(start, end, property_id="359129908"):
 
         
     )
-    response = client.run_report(request)
-    data, rows_retuned = parseReport(response, True, 'sessions') 
-    df = pd.DataFrame(data, columns=['eventName','PagePath', 'eventValue', 'eventCount'])
-    print(df.head())
+    response = client.run_report(request)   
+    data, rows_retuned = parseReport(response, 'eventName', 'sessions') 
+    df = pd.DataFrame(data, columns=['eventName','eventValue', 'eventCount'])
+    print(df.head())    
 getRawReport("2023-04-01","today")
-monthly_usage("2023-04-01","today", csv_file=None)
+total_usage("2023-04-01","today", csv_file='usage.csv')
+by_country( '2023-04-01','2023-04-30', csv_file= None)
