1d4423e87018e2b541b14d7bc7b9995f80602133
Feisal01 <feisal_darar@hotmail.com>
2023-11-17 14:16:31 -0500

    update

--- pd_vis.py
+++ pd_vis.py
@@ -11,7 +11,7 @@ col_melt = ["ati_all","ati_nil","contracts","contracts_nil","contractsa","grants
              ,"reclassification_nil","travela","travelq","travelq_nil","hospitalityq","hospitalityq_nil","dac",
              "briefingt","qpnotes","wrongdoing","adminaircraft"]
 sel_col =["ati_all","contracts", "grants", "travelq", "hospitalityq" ]
-df_melt = pd.melt(df, id_vars=['date'], value_vars=sel_col) 
+df_melt = pd.melt(df.head(15), id_vars=['date'], value_vars=sel_col) 
 df_melt.rename(columns={"variable":"pd_type",
                         "value":"count"}, inplace=True)
 df_melt.sort_values(by=['date'], axis=0, ascending=False, inplace=True)
@@ -23,7 +23,7 @@ fig.update_layout(
     font_color="blue",
     title_font_family="Times New Roman",
     title_font_color="blue", 
-    title_text = "New ATI, contracts, grants, travel and hospitality",
+    title_text = "New ATI, contracts, grants, travel and hospitality in last 15 days",
     title_font_size = 24,
     title_x= 0.5,   
     legend_title_font_color="green"
