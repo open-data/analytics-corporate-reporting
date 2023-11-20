95ec5184bfac1e031f36553b043dc53ccaa40059
Feisal01 <feisal_darar@hotmail.com>
2023-11-20 09:47:41 -0500

    update

--- pd_vis.py
+++ pd_vis.py
@@ -1,32 +1,33 @@
 import plotly.express as px
 import pandas as pd
 
-
 df = pd.read_csv("structure_pd.csv", encoding="utf-8")
-base_val =[("ati_all",165646),("contracts",1036237),("grants",949177),("travelq",91093),("hospitalityq",22449)]
-for lab,val in base_val:
+df.rename(columns={"ati_all": "ATI", "contracts": "Contracts", "grants": "Grants",
+          "travelq": "Travel", "hospitalityq": "Hospitality"}, inplace=True)
+base_val = [("ATI", 165646), ("Contracts", 1036237), ("Grants",
+                                                      949177), ("Travel", 91093), ("Hospitality", 22449)]
+for lab, val in base_val:
     df[lab] = df[lab]-val
-df.drop([len(df)-1], inplace=True) 
-col_melt = ["ati_all","ati_nil","contracts","contracts_nil","contractsa","grants","grants_nil","reclassification"
-             ,"reclassification_nil","travela","travelq","travelq_nil","hospitalityq","hospitalityq_nil","dac",
-             "briefingt","qpnotes","wrongdoing","adminaircraft"]
-sel_col =["ati_all","contracts", "grants", "travelq", "hospitalityq" ]
-df_melt = pd.melt(df.head(15), id_vars=['date'], value_vars=sel_col) 
-df_melt.rename(columns={"variable":"pd_type",
-                        "value":"count"}, inplace=True)
+df.drop([len(df)-1], inplace=True)
+sel_col = ["ATI", "Contracts", "Grants", "Travel", "Hospitality"]
+df_melt = pd.melt(df.head(15), id_vars=['date'], value_vars=sel_col)
+df_melt.rename(columns={"variable": "pd_type",
+                        "value": "count"}, inplace=True)
 df_melt.sort_values(by=['date'], axis=0, ascending=False, inplace=True)
-fig=px.bar(df_melt,x="date", y="count", color ="pd_type", barmode="group",text_auto='.2s')
-fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+fig = px.bar(df_melt, x="date", y="count", color="pd_type",
+             barmode="group", text_auto='.2s')
+fig.update_traces(textfont_size=12, textangle=0,
+                  textposition="outside", cliponaxis=False)
 
 fig.update_layout(
     font_family="Courier New",
     font_color="blue",
     title_font_family="Times New Roman",
-    title_font_color="blue", 
-    title_text = "New ATI, contracts, grants, travel and hospitality in last 15 days",
-    title_font_size = 24,
-    title_x= 0.5,   
+    title_font_color="blue",
+    title_text="New ATI, contracts, grants, travel and hospitality in last 15 days",
+    title_font_size=24,
+    title_x=0.5,
     legend_title_font_color="green"
 )
 fig.show()
-fig.write_image("plot.svg", width=800, height = 500)
+fig.write_image("plot.svg")
