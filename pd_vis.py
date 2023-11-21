766deb214164122ec59e0f02c6b673aac445ccd7
Feisal01 <feisal_darar@hotmail.com>
2023-11-21 11:03:01 -0500

    plot update

--- pd_vis.py
+++ pd_vis.py
@@ -4,8 +4,9 @@ import pandas as pd
 df = pd.read_csv("structure_pd.csv", encoding="utf-8")
 df.rename(columns={"ati_all": "ATI", "contracts": "Contracts", "grants": "Grants",
           "travelq": "Travel", "hospitalityq": "Hospitality"}, inplace=True)
-base_val = [("ATI", 165646), ("Contracts", 1036237), ("Grants",
-                                                      949177), ("Travel", 91093), ("Hospitality", 22449)]
+df = df.head(7)
+base_val = [("ATI", df["ATI"][6]), ("Contracts", df["Contracts"][6]), ("Grants", df["Grants"][6]),
+            ("Travel", df["Travel"][6]), ("Hospitality", df["Hospitality"][6])]
 for lab, val in base_val:
     df[lab] = df[lab]-val
 df.drop([len(df)-1], inplace=True)
@@ -15,7 +16,7 @@ df_melt.rename(columns={"variable": "pd_type",
                         "value": "count"}, inplace=True)
 df_melt.sort_values(by=['date'], axis=0, ascending=False, inplace=True)
 fig = px.bar(df_melt, x="date", y="count", color="pd_type",
-             barmode="group", text_auto='.2s')
+             barmode="group", text_auto='s')
 fig.update_traces(textfont_size=12, textangle=0,
                   textposition="outside", cliponaxis=False)
 
@@ -24,10 +25,11 @@ fig.update_layout(
     font_color="blue",
     title_font_family="Times New Roman",
     title_font_color="blue",
-    title_text="New ATI, contracts, grants, travel and hospitality in last 15 days",
+    title_text="New ATI, contracts, grants, travel and hospitality in last 7 days",
     title_font_size=24,
     title_x=0.5,
-    legend_title_font_color="green"
+    legend_title_text= "PD types",    
+    legend_title_font_color="blue"
 )
-fig.show()
+# fig.show()
 fig.write_image("plot.svg")
